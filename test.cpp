2017529ead5d19a80c68f332a98c5fadae8f5ef0
Tuan Phan <tphan@seattleu.edu>
2019-11-05 23:05:05 -0800

    Add test.cpp file

---
+++ test.cpp
@@ -0,0 +1,18 @@
+//
+//  test.cpp
+//  BST
+//
+//  Created by Tuan Phan on 11/5/19.
+//  Copyright © 2019 Tuan Phan. All rights reserved.
+//
+
+#include <stdio.h>
+using namspace std;
+
+int test()
+{
+    
+    cout << "this is a test file" << endl;
+    
+    return 0;
+}
