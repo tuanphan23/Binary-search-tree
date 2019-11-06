ea2d49a64201b1e91eac658c2a0e5c3f0f593ce5
Tuan Phan <tphan@seattleu.edu>
2019-11-05 23:23:25 -0800

    Testing branch in GitHub

---
+++ testing-branch.cpp
@@ -0,0 +1,15 @@
+//
+//  testing-branch.cpp
+//  BST
+//
+//  Created by Tuan Phan on 11/5/19.
+//  Copyright © 2019 Tuan Phan. All rights reserved.
+//
+
+#include <stdio.h>
+using namespace std;
+
+int testingBranch()
+{
+    cout << "testing branch in GitHub" << endl;
+}
